2581a4128ce7a2965209cad7a088183dea848fdf
Pascal Bodin <pascal.bodin@systev.com>
2023-12-16 16:33:09 +0100

    Add exercise 2

---
+++ exercises/02-ledHandling/main/main.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+void app_main(void)
+{
+
+}
